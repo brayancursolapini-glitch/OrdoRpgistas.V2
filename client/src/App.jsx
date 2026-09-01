8f0be0f31362c200866483e55393e6989f06bb14
brayancursolapini-glitch <brayancursolapini@gmail.com>
2026-08-31 22:16:29 -0300

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+
+import IntroLoader from "./components/Loading/IntroLoader";
+
+import Landing from "./pages/Landing/Landing";
+import Login from "./pages/Login/Login";
+import Cadastro from "./pages/Cadastro/Cadastro";
+import Home from "./pages/Home/Home";
+
+export default function App() {
+
+    const [loading, setLoading] =
+        useState(true);
+
+    const [currentPage, setCurrentPage] =
+        useState("landing");
+
+
+    if (loading) {
+
+        return (
+            <IntroLoader
+                onComplete={() => {
+                    setLoading(false);
+                }}
+            />
+        );
+
+    }
+
+
+    if (currentPage === "landing") {
+
+        return (
+            <Landing
+                onLogin={() => {
+                    setCurrentPage("login");
+                }}
+                onRegister={() => {
+                    setCurrentPage("cadastro");
+                }}
+            />
+        );
+
+    }
+
+
+    if (currentPage === "login") {
+
+        return (
+            <Login
+                onBack={() => {
+                    setCurrentPage("landing");
+                }}
+                onRegister={() => {
+                    setCurrentPage("cadastro");
+                }}
+                onLoginSuccess={() => {
+                    setCurrentPage("home");
+                }}
+            />
+        );
+
+    }
+
+
+    if (currentPage === "cadastro") {
+
+        return (
+            <Cadastro
+                onBack={() => {
+                    setCurrentPage("landing");
+                }}
+                onLogin={() => {
+                    setCurrentPage("login");
+                }}
+                onRegisterSuccess={() => {
+                    setCurrentPage("home");
+                }}
+            />
+        );
+
+    }
+
+
+    if (currentPage === "home") {
+
+        return (
+            <Home
+                onLogout={() => {
+                    setCurrentPage("landing");
+                }}
+            />
+        );
+
+    }
+
+
+    return (
+        <Landing
+            onLogin={() => {
+                setCurrentPage("login");
+            }}
+            onRegister={() => {
+                setCurrentPage("cadastro");
+            }}
+        />
+    );
+
+}
