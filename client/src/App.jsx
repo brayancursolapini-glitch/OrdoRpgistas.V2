71e0c34d9b23395e05cb02502249f09f3d4235cf
brayancursolapini-glitch <brayancursolapini@gmail.com>
2026-09-04 09:22:36 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,218 +1,54 @@
-import {
-    useState,
-} from "react";
+import { useState } from "react";
 
-import IntroLoader
-    from "./components/Loading/IntroLoader";
+import Landing from "./pages/Landing/Landing";
+import Login from "./pages/Login/Login";
+import Cadastro from "./pages/Cadastro/Cadastro";
+import Home from "./pages/Home/Home";
 
-import Landing
-    from "./pages/Landing/Landing";
+import IntroLoader from "./components/Loading/IntroLoader/IntroLoader";
 
-import Login
-    from "./pages/Login/Login";
+function App() {
+  const [currentPage, setCurrentPage] = useState("landing");
+  const [loading, setLoading] = useState(true);
 
-import Cadastro
-    from "./pages/Cadastro/Cadastro";
-
-import Home
-    from "./pages/Home/Home";
-
-
-export default function App() {
-
-    const [
-        currentPage,
-        setCurrentPage,
-    ] = useState(
-        "landing"
-    );
-
-
-    const [
-        loading,
-        setLoading,
-    ] = useState(
-        true
-    );
-
-
-    if (loading) {
-
-        return (
-
-            <IntroLoader
-
-                onComplete={() => {
-
-                    setLoading(false);
-
-                }}
-
-            />
-
-        );
-
-    }
-
-
-    if (
-        currentPage ===
-        "landing"
-    ) {
-
-        return (
-
-            <Landing
-
-                onLogin={() => {
-
-                    setCurrentPage(
-                        "login"
-                    );
-
-                }}
-
-                onRegister={() => {
-
-                    setCurrentPage(
-                        "cadastro"
-                    );
-
-                }}
-
-            />
-
-        );
-
-    }
-
-
-    if (
-        currentPage ===
-        "login"
-    ) {
-
-        return (
-
-            <Login
-
-                onBack={() => {
-
-                    setCurrentPage(
-                        "landing"
-                    );
-
-                }}
-
-                onRegister={() => {
-
-                    setCurrentPage(
-                        "cadastro"
-                    );
-
-                }}
-
-                onLoginSuccess={() => {
-
-                    setCurrentPage(
-                        "home"
-                    );
-
-                }}
-
-            />
-
-        );
-
-    }
-
-
-    if (
-        currentPage ===
-        "cadastro"
-    ) {
-
-        return (
-
-            <Cadastro
-
-                onBack={() => {
-
-                    setCurrentPage(
-                        "landing"
-                    );
-
-                }}
-
-                onLogin={() => {
-
-                    setCurrentPage(
-                        "login"
-                    );
-
-                }}
-
-                onRegisterSuccess={() => {
-
-                    setCurrentPage(
-                        "home"
-                    );
-
-                }}
-
-            />
-
-        );
-
-    }
-
-
-    if (
-        currentPage ===
-        "home"
-    ) {
-
-        return (
-
-            <Home
-
-                onLogout={() => {
-
-                    setCurrentPage(
-                        "landing"
-                    );
-
-                }}
-
-            />
-
-        );
-
-    }
-
-
+  if (loading) {
     return (
-
-        <Landing
-
-            onLogin={() => {
-
-                setCurrentPage(
-                    "login"
-                );
-
-            }}
-
-            onRegister={() => {
-
-                setCurrentPage(
-                    "cadastro"
-                );
-
-            }}
-
-        />
-
+      <IntroLoader
+        onFinish={() => setLoading(false)}
+      />
     );
+  }
 
+  switch (currentPage) {
+    case "login":
+      return (
+        <Login
+          setCurrentPage={setCurrentPage}
+        />
+      );
+
+    case "cadastro":
+      return (
+        <Cadastro
+          setCurrentPage={setCurrentPage}
+        />
+      );
+
+    case "home":
+      return (
+        <Home
+          setCurrentPage={setCurrentPage}
+        />
+      );
+
+    case "landing":
+    default:
+      return (
+        <Landing
+          setCurrentPage={setCurrentPage}
+        />
+      );
+  }
 }
+
+export default App;
