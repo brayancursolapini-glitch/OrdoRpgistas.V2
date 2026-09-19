8b40c5435c2617b5924ff0e3b18b75876696eee6
brayancursolapini-glitch <brayancursolapini@gmail.com>
2026-09-19 16:07:02 -0300

    Refactor main.jsx to include AudioProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,40 +1,22 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import {
-    ThemeProvider,
-} from "./context/ThemeContext";
-
-import {
-    UserProvider,
-} from "./context/UserContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { UserProvider } from "./context/UserContext";
+import { AudioProvider } from "./context/AudioContext";
 
 import "./index.css";
 
-
-ReactDOM
-    .createRoot(
-        document.getElementById(
-            "root"
-        )
-    )
-    .render(
-
-        <React.StrictMode>
-
-            <UserProvider>
-
-                <ThemeProvider>
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <UserProvider>
+            <ThemeProvider>
+                <AudioProvider>
                     <App />
-
-                </ThemeProvider>
-
-            </UserProvider>
-
-        </React.StrictMode>
-
-    );
+                </AudioProvider>
+            </ThemeProvider>
+        </UserProvider>
+    </React.StrictMode>
+);
