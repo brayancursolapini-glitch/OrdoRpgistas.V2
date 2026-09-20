8038b83afbad7987418833d65f1f5a1b468780a6
brayancursolapini-glitch <brayancursolapini@gmail.com>
2026-09-19 21:30:19 -0300

    Import page-placeholder.css in ProcurarJogadores
    Added import for page-placeholder.css to style the component.

--- client/src/pages/ProcurarJogadores/ProcurarJogadores.jsx
+++ client/src/pages/ProcurarJogadores/ProcurarJogadores.jsx
@@ -1,6 +1,7 @@
 import { Search } from "lucide-react";
 
 import PageBase from "../PageBase";
+import "../page-placeholder.css";
 
 export default function ProcurarJogadores({ onNavigate }) {
     return (
